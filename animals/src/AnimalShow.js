3cff5fd4d3887fe7dad863f35c24fd30c626846c
Saif <saif@Arifs-MBP-4.lan>
2023-10-09 17:36:57 -0400

    Adding images and altering sizes of images

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,6 +1,41 @@
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    //object when key is identical to the value
+    bird,  // equivalent to bird: bird
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
 // Pass down a prop called type
 function AnimalShow({ type }) {
-    <div>{type}</div>   // Prints out whatever that prop is
+    // Track the number of clicks
+    const [clicks, setClicks] = useState(0);
+
+    //Watch for click events (in this case, its the div below)
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
+    return(
+        <div onClick={handleClick}>
+            <img alt="animal" src={svgMap[type]} />
+            <img alt="heart" src={heart} 
+            style = {{width: 10 + 10 * clicks + 'px'}} // One set to indicate we're writing JS, second set to indicate we are creating an object
+            /> 
+        </div> 
+    );  
 }
 
 export default AnimalShow;
+
