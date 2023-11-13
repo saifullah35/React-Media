c5da3b98d03cec9864381379aeca39e3e08df68b
Saif <saif@Arifs-MBP-4.lan>
2023-11-13 15:55:14 -0500

    awkward double keys

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,5 +1,11 @@
+import { useSelector } from 'react-redux';
+
 function CarList() {
-    return <div>CarForm</div>
+    const cars = useSelector((state) => {
+        return state.cars.data;
+    });
+
+    return <div>CarList</div>
 }
 
 export default CarList;
