3ad2b064a48abd44c8020ca90f0eb3f699b171cb
Saif <saif@Arifs-MBP-4.lan>
2023-11-16 10:23:02 -0500

    wrapping up the thunk

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "../store";
+
 function UsersList() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUser());
+    }, [dispatch]);
+    
     return 'Users List';
 }
 
