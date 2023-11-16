78ed934d96a2fa4a9aefb9e9dfba190af4f32cd4
Saif <saif@Arifs-MBP-4.lan>
2023-11-16 11:13:10 -0500

    adding a pause for texting

--- media/src/store/thunks/fetchUser.js
+++ media/src/store/thunks/fetchUser.js
@@ -4,7 +4,16 @@ import axios from "axios";
 const fetchUser = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
     
+    await pause(1000);
+
     return response.data;
 });
+
+//DEV ONLY!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+}
 
 export { fetchUser };
