edbacea3d0fbea26d1dea9cf9c54e703e3254369
Saif <saif@Arifs-MBP-4.lan>
2023-11-16 10:13:18 -0500

    More on adding thunks

--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -1,11 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUser } from "../thunks/fetchUser";
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
-        data: []
+        data: [],
+        isLoading: false,
+        error: null,
     },
-    reducers: {}
+    extraReducers(builder) {
+        builder.addCase(fetchUser.pending, (state, action) => {
+            // Update our state object however appropriate
+            // to show the user what we are loading data
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+    }
 });
 
 export const usersReducer = usersSlice.reducer;
