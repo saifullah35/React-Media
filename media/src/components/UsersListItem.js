cc63f9393311f513c3dc1691ca0388884850e67c
Saif <saif@Arifs-MBP-4.lan>
2023-11-20 17:49:59 -0500

    Deleting the user

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -1,9 +1,24 @@
+import { GoTrashCan } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
 function UsersListItem({ user }) {
+    const [ doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+    const handleClick = () => {
+        doRemoveUser(user);
+    };
+
     return (
             <div className="mb-2 border rounded">
-                    <div className="flex p-2 justify-between items-cenetr cursor-pointer">
-                        {user.name}
-                    </div>
+                <div className="flex p-2 justify-between items-cenetr cursor-pointer">
+                    <Button loading={isLoading} onClick={handleClick}>
+                        <GoTrashCan />
+                    </Button>
+                    {error && <div> Error deleting user. </div>}
+                    {user.name}
+                </div>
             </div>
         );
 }
